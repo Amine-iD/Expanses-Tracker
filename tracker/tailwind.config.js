34c587466082e22e633b7f1f4cd177359e276dec
Amine <amineidelkadi@gmail.com>
2024-04-20 16:54:35 +0100

    Reorganize the project structure

---
+++ tracker/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+            "./templates/*.html",],
+  theme: {
+    extend: {},
+    colors:{
+      body:'#f0f4f8',
+      nav : '#2d3748',
+      link:'#ffffff',
+      button:'#48bb78',
+      button_hover:'#38a169'
+    }
+  },
+  plugins: [],
+}
