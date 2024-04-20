0452d46df1c4e8cbdcbd54f2bcb575accae7ae8b
Amine <amineidelkadi@gmail.com>
2024-04-20 16:56:44 +0100

    Make the tracker directory as a package

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 """  I do not know if this file will be used or not . If yes, in what ?   """
-from views import create_app
+from flask import Flask
+import tracker.pages as pages
 
-app = create_app()
-app.run(debug=True)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages.bp)
+    return app
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
