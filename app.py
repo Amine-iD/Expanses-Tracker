7277f61ab85e12cb1dafa0b1871de5e735575019
Amine <amineidelkadi@gmail.com>
2024-04-21 12:28:29 +0100

    Make the main code runner file of the project

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 """  I do not know if this file will be used or not . If yes, in what ?   """
-from flask import Flask
-import tracker.pages as pages
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(pages.bp)
-    return app
+from tracker import create_app
+
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
