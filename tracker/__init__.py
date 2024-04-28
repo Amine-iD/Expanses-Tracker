c229b4fc1f5c3c1c1ee785e77fe000a169cdb44e
Amine <amineidelkadi@gmail.com>
2024-04-28 11:01:03 +0100

    Add the csrf secret key

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , current_app
 from flask_sqlalchemy import SQLAlchemy
 from tracker import routes
 
@@ -6,7 +6,9 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(routes.bp)
     return app
+
 app = create_app()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tracker.db' 
+app.config['SECRET_KEY'] = 'b9212131a5fe1e1345346de59f1cab71c17941e23fb166dc52ba162a3ca78c2e'
 db = SQLAlchemy(app)
