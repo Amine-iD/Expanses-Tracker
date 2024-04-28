80a5eda41eb3c8c4236785183bab20098114c03e
Amine <amineidelkadi@gmail.com>
2024-04-28 11:11:51 +0100

    Add the email field to the User table

--- tracker/dbmodels.py
+++ tracker/dbmodels.py
@@ -1,16 +1,33 @@
 from tracker import db 
+from tracker import app
 from datetime import datetime
+# from sqlalchemy import 
+
 
 class User(db.Model):
     user_id = db.Column(db.Integer() , primary_key = True)
     user_name = db.Column(db.String(length = 20) , nullable = False , unique = True)
-    balances = db.relationship('Balance', backref = 'author' , lazy = True)
+    email = db.Column(db.String() , nullable = False , unique = True)
+    balances = db.relationship('Balance', backref = 'owner' , lazy = True) # different balances for diffrent months
 
 class Category(db.Model):
     category_id = db.Column(db.Integer() , primary_key = True)
     category_name = db.Column(db.String() , nullable = False , unique = True)
+    items = db.relationship('Item' , backref = 'item-category' , lazy = True)
 
 class Balance(db.Model):
+    id = db.Column(db.Integer() , primary_key = True)
     amount = db.Column(db.Integer() , nullable = True)
     period = db.Column(db.String(length = 30) , nullable = False , unique = True) 
-    user_id = db.Column(db.Integer() , db.ForeignerKey('User.user_id')) 
+    user_id = db.Column(db.Integer() , db.ForeignKey('user.user_id'))
+
+class Item(db.Model):
+    item_id = db.Column(db.Integer() , primary_key = True)
+    item_name = db.Column(db.String(length = 50) , nullable = False , unique = True)
+    item_category = db.Column(db.String() , db.ForeignKey('category.category_id'))
+    amount = db.Column(db.Integer)  
+
+# This is how we converted the classes into db tables and connected them to the db file. but we needed to run this 
+# file `dbmodels.py` seperately in order to do so.
+with app.app_context():
+    db.create_all()
--- tracker/routes.py
+++ tracker/routes.py
@@ -1,10 +1,21 @@
 from flask import Blueprint , render_template
+from tracker.forms import Register
+from tracker.dbmodels import User
 
 bp = Blueprint('pages' , __name__)
 
 @bp.route('/')
 def home():
     return render_template('home.html')
+
 @bp.route('/about')
 def about():
     return render_template('about.html')
+
+@bp.route('/register')
+def register():
+    form = Register() # This allows us to make conditions upon the redirected template 
+    user_to_create = User(
+        user_name = form.user_name,
+    ) 
+    return render_template('register.html' , form = form)
